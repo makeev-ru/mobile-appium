21b30aa25989052f4260ba204f38849052e8e127
Ruslan Pryimak <rpryimak@lyft.com>
2023-11-30 23:41:25 +0100

    added tc_3

---
+++ src/test/java/appiumproject/eCommerce_TC_3.java
@@ -0,0 +1,54 @@
+package appiumproject;
+
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class eCommerce_TC_3 extends BaseTest {
+
+    @Test
+    public void AddToCart() throws InterruptedException {
+        driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("TestName");
+        driver.hideKeyboard();
+        driver.findElement(By.id("com.androidsample.generalstore:id/radioFemale")).click();
+        driver.findElement(By.id("android:id/text1")).click();
+        driver.findElement(AppiumBy.androidUIAutomator(
+                "new UiScrollable(new UiSelector()).scrollIntoView(text(\"Belarus\"));"));
+        driver.findElement(By.xpath("//android.widget.TextView[@text='Belarus']")).click();
+        driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+        driver.findElements(By.xpath("//android.widget.TextView[@text='ADD TO CART']")).get(0).click();
+        driver.findElements(By.xpath("//android.widget.TextView[@text='ADD TO CART']")).get(0).click();
+        driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.attributeContains(driver.findElement(By.id("com.androidsample.generalstore:id/toolbar_title")), "text", "Cart"));
+
+        List<WebElement> productPrices = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice"));
+        int count = productPrices.size();
+        Double totalSum = 0.0;
+
+        for (int i = 0; i < count; i++) {
+            String amountString = productPrices.get(i).getText();
+            Double price = getFormattedAmount(amountString);
+            totalSum+=price;
+        }
+        String displaySum = driver.findElement(By.id("com.androidsample.generalstore:id/totalAmountLbl")).getText();
+        Double displayFormattedSum = getFormattedAmount(displaySum);
+
+        Assert.assertEquals(totalSum, displayFormattedSum);
+
+        WebElement ele = driver.findElement(By.id("com.androidsample.generalstore:id/termsButton"));
+        longPressAction(ele);
+        driver.findElement(By.id("android:id/button1")).click();
+        driver.findElement(AppiumBy.className("android.widget.CheckBox")).click();
+        driver.findElement(By.id("com.androidsample.generalstore:id/btnProceed")).click();
+        Thread.sleep(3000);
+    }
+}
