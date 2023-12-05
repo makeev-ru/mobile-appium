94094bd0915ffd804acb07117746a5d615113bfb
Ruslan Pryimak <rpryimak@lyft.com>
2023-12-06 00:57:10 +0100

    add first iOS test with configuration

---
+++ src/test/java/iOS/iOSBaseTest.java
@@ -0,0 +1,47 @@
+package iOS;
+
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.options.XCUITestOptions;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class iOSBaseTest {
+
+    public IOSDriver driver;
+    public AppiumDriverLocalService service;
+
+    @BeforeClass
+    public void configureAppium() throws MalformedURLException {
+        service = new AppiumServiceBuilder()
+                .withAppiumJS(new File("//opt/homebrew//bin//appium"))
+                .usingDriverExecutable(new File("//opt/homebrew//bin//node"))
+                .withIPAddress("127.0.0.1")
+                .usingPort(4723)
+                .build();
+        service.start();
+
+        XCUITestOptions options = new XCUITestOptions();
+        options.setDeviceName("iPhone 15");
+        options.setApp("/Users/rpryimak/Library/Developer/Xcode/DerivedData/UIKitCatalog-diyqesxmimboqcfylmflgyyixubd/Build/Products/Debug-iphonesimulator/UIKitCatalog.app");
+        options.setPlatformName("17.0");
+        options.setWdaLaunchTimeout(Duration.ofSeconds(20));
+
+        String service_url = service.getUrl().toString();
+        driver = new IOSDriver(new URL(service_url), options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+        service.stop();
+    }
+}
---
+++ src/test/java/iOS/iOSBasics.java
@@ -0,0 +1,23 @@
+package iOS;
+
+import io.appium.java_client.AppiumBy;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class iOSBasics extends iOSBaseTest {
+    @Test
+    public void iOSBasicTest() {
+        driver.findElement(AppiumBy.accessibilityId("Alert Views")).click();
+        driver.findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeStaticText[`name == 'Text Entry'`]")).click();
+        driver.findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeCell")).sendKeys("Hello");
+        driver.findElement(AppiumBy.accessibilityId("OK")).click();
+
+        driver.findElement(AppiumBy.iOSNsPredicateString("type == 'XCUIElementTypeStaticText' AND value BEGINSWITH[c] 'Confirm'")).click();
+
+        String actualText = driver.findElement(AppiumBy.iOSNsPredicateString("type == 'XCUIElementTypeStaticText' AND name BEGINSWITH[c] 'A message'")).getText();
+        String expectedText = "A message should be a short, complete sentence.";
+        Assert.assertEquals(actualText, expectedText);
+
+        driver.findElement(AppiumBy.iOSNsPredicateString("name == 'Confirm'"));
+    }
+}
