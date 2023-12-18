0b56f7218a378e90ccc063703f7d8877ae53664d
Ruslan Pryimak <rpryimak@lyft.com>
2023-12-18 01:16:01 +0100

    refactor tc_2 based on page object model

--- src/test/java/android/eCommerce_TC_2.java
+++ src/test/java/android/eCommerce_TC_2.java
@@ -1,6 +1,8 @@
 package android;
 
-import io.appium.java_client.AppiumBy;
+import org.example.pageObjects.android.CartPage;
+import org.example.pageObjects.android.FormPage;
+import org.example.pageObjects.android.ProductCatalogue;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -13,36 +15,19 @@ public class eCommerce_TC_2 extends androidConfig {
 
     @Test
     public void AddToCart() throws InterruptedException {
-        driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("TestName");
-        driver.hideKeyboard();
-        driver.findElement(By.id("com.androidsample.generalstore:id/radioFemale")).click();
 
-        driver.findElement(By.id("android:id/text1")).click();
-        driver.findElement(AppiumBy.androidUIAutomator(
-                "new UiScrollable(new UiSelector()).scrollIntoView(text(\"Belarus\"));"));
-        driver.findElement(By.xpath("//android.widget.TextView[@text='Belarus']")).click();
-        driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+        FormPage formPage = new FormPage(driver);
+        formPage.setNameField("TestName");
+        formPage.setGender("female");
+        formPage.setCountry("Belarus");
+        ProductCatalogue productCatalogue = formPage.pressShopButton();
 
-        driver.findElement(AppiumBy.androidUIAutomator(
-                "new UiScrollable(new UiSelector()).scrollIntoView(text(\"Jordan 6 Rings\"));"));
-
-        int productCount = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).size();
-
-        for (int i = 0; i < productCount; i++) {
-            String productName = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).get(i).getText();
-
-            if (productName.equalsIgnoreCase("Jordan 6 Rings")) {
-                driver.findElements(By.id("com.androidsample.generalstore:id/productAddCart")).get(i).click();
-
-            }
-
-        }
-        driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
+        CartPage cartPage = productCatalogue.addProductToCart("Jordan 6 Rings");
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.attributeContains(driver.findElement(By.id("com.androidsample.generalstore:id/toolbar_title")), "text", "Cart"));
 
-        String productNameInCart = driver.findElement(By.id("com.androidsample.generalstore:id/productName")).getText();
+        String productNameInCart = cartPage.getProductNameInCart();
         Assert.assertEquals(productNameInCart, "Jordan 6 Rings");
     }
 }
