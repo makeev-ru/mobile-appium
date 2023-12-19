de3b940b909111af5ffaea853f3ee1a9ef4e8cc8
Ruslan Pryimak <rpryimak@lyft.com>
2023-12-19 15:38:09 +0100

    create page object model for ios

---
+++ src/main/java/org/example/pageObjects/iOS/AlertViews.java
@@ -0,0 +1,52 @@
+package org.example.pageObjects.iOS;
+
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.appium.java_client.pagefactory.iOSXCUITFindBy;
+import org.example.utils.IOSActions;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+public class AlertViews extends IOSActions {
+
+    IOSDriver driver;
+
+    public AlertViews(IOSDriver driver) {
+        super(driver);
+        this.driver = driver;
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+    }
+
+    @iOSXCUITFindBy(iOSClassChain="**/XCUIElementTypeStaticText[`name == 'Text Entry'`]")
+    private WebElement textEntryMenu;
+
+    @iOSXCUITFindBy(iOSNsPredicate="type == 'XCUIElementTypeStaticText' AND value BEGINSWITH[c] 'Confirm'")
+    private WebElement confirmMenuItem;
+
+    @iOSXCUITFindBy(iOSClassChain="**/XCUIElementTypeCell")
+    private WebElement textBox;
+
+    @iOSXCUITFindBy(accessibility="OK")
+    private WebElement acceptPopup;
+
+    @iOSXCUITFindBy(iOSNsPredicate="name BEGINSWITH[c] 'A message'")
+    private WebElement confirmMessage;
+
+    @iOSXCUITFindBy(iOSNsPredicate="label == 'Confirm'")
+    private WebElement submit;
+
+    public void fillTextLabel(String name){
+        textEntryMenu.click();
+        textBox.sendKeys(name);
+        acceptPopup.click();
+    }
+
+    public String getConfirmMessage(){
+        confirmMenuItem.click();
+        return confirmMessage.getText();
+    }
+
+    public void submitForm(){
+        submit.click();
+    }
+}
